498a70fb76b83d9d7e9a9a2b687c6a8c7a71a650
Fatih Koç <mail@fatihkoc.net>
2022-08-17 21:14:47 +0300

    add cypress.config.js

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,5 +1,5 @@
 resource "aws_acm_certificate" "cert" {
-  provider                  = aws.us-east
+  provider                  = aws.us-east # CloudFront only supports certificates in us-east-1
   domain_name               = var.domain_name
   subject_alternative_names = ["*.fatihkoc.net"]
   validation_method         = "DNS"
@@ -11,9 +11,10 @@ resource "aws_acm_certificate" "cert" {
   depends_on = [aws_route53_zone.primary]
 }
 
+# Create a DNS record to prove that we _own_ the domain we're requesting a certificate for
 resource "aws_route53_record" "certvalidation" {
 
-  provider = aws.us-east
+  provider = aws.us-east # Cloudfront only supports us-east-1
 
   for_each = {
     for d in aws_acm_certificate.cert.domain_validation_options : d.domain_name => {
@@ -31,12 +32,14 @@ resource "aws_route53_record" "certvalidation" {
   zone_id         = aws_route53_zone.primary.zone_id
 }
 
+# Validate the certificate
 resource "aws_acm_certificate_validation" "certvalidation" {
-  provider                = aws.us-east
+  provider                = aws.us-east # Cloudfront only supports us-east-1
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for r in aws_route53_record.certvalidation : r.fqdn]
 }
 
+# Create Route53 record for the domain
 resource "aws_route53_record" "websiteurl" {
 
   depends_on = [
