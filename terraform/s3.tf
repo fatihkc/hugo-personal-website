807f585fcd98f55d13096e8414cca5b6f5260654
Fatih Koç <mail@fatihkoc.net>
2025-09-08 23:54:05 +0300

    update s3 policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,31 +1,14 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   bucket = "fatihkocnet-hugo"
-  versioning = {
-    enabled = true
-  }
-
-  # Block all public access
+  # Block all public access for security
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-
-  # Bucket policy to allow only CloudFront OAI access
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "AllowCloudFrontServicePrincipal"
-        Effect    = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${module.cdn.cloudfront_origin_access_identity_ids.s3_bucket_one}"
-        }
-        Action   = "s3:GetObject"
-        Resource = "${module.s3_bucket.s3_bucket_arn}/*"
-      }
-    ]
-  })
+  versioning = {
+    enabled = true
+  }
 
   website = {
     index_document = "index.html"
@@ -39,4 +22,24 @@ module "s3_bucket" {
       }
     }]
   }
+}
+
+# S3 bucket policy to allow only CloudFront OAI access
+resource "aws_s3_bucket_policy" "cloudfront_access" {
+  bucket = module.s3_bucket.s3_bucket_id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontServicePrincipal"
+        Effect = "Allow"
+        Principal = {
+          AWS = module.cdn.cloudfront_origin_access_identity_iam_arns[0]
+        }
+        Action   = "s3:GetObject"
+        Resource = "${module.s3_bucket.s3_bucket_arn}/*"
+      }
+    ]
+  })
 }
