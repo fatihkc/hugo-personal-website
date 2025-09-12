c02c9b8e56c6756249ef9ab8c90c14dce3540e89
Fatih Koç <mail@fatihkoc.net>
2025-09-12 15:40:06 +0300

    www to non-www

--- terraform/scripts/redirect.js
+++ terraform/scripts/redirect.js
@@ -1,6 +1,21 @@
 function handler(event) {
     var request = event.request;
     var uri = request.uri;
+    var host = request.headers.host.value;
+    
+    // Redirect www to non-www (canonical URL)
+    if (host.startsWith('www.')) {
+        var canonicalHost = host.substring(4); // Remove 'www.' prefix
+        return {
+            statusCode: 301,
+            statusDescription: 'Moved Permanently',
+            headers: {
+                'location': {
+                    value: 'https://' + canonicalHost + uri
+                }
+            }
+        };
+    }
     
     // Check whether the URI is missing a file name.
     if (uri.endsWith('/')) {
